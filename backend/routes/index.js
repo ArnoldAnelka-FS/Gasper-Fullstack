1f5e5d839e9f84d6713777b97553cd1c625eefd3
Anelka <aaarnold@student.fullsail.edu>
2024-05-05 04:14:06 -0400

    index.js updates

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Welcome to the Gasper API');
+  res.send('Welcome to the Gasper API- Let us start changing the way we fuel!');
 });
 
 module.exports = router;
